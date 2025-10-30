716e448bad2a14b0d9effefd77274eff28103f32
Keisuke Taniguchi <rememberthatk@icloud.com>
2025-10-30 11:47:49 +0900

    add button;

--- vite-blueprint/src/App.tsx
+++ vite-blueprint/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <div>
       <p className="text-3xl text-red-500 underline">hello</p>
+      <Button>Click me</Button>
     </div>
   )
 }
